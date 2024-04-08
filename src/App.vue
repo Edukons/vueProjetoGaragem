908f761f73eb90bd0d77db195432c4cacc8a7d8c
Edukons <eduardo.kons03@gmail.com>
2024-04-08 15:02:05 -0300

    Concluido com erros

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@ import router from './router';
 <template>
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/categorias">Categorias</router-link>
+    <router-link to="/categorias">Cores</router-link>
+    <router-link to="/categorias">Marcas</router-link>
+    <router-link to="/categorias">Acessorios</router-link>
   </header>
   <main>
     <RouterView />
